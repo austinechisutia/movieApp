1548d8315cd5ca840682bde4fcb2c85aca89931f
austinechisutia <austineakhonya624@gmail.com>
2025-02-04 13:24:06 +0300

    put the movies on the screen using an api

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 $(document).ready(()=>{
     $('#searchForm').on('submit', (e)=>{
-        console.log($('#searchText').val());
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
         e.preventDefault();
-    })
-})
+    });
+});
+
+function getMovies(searchText) {
+    axios.get(`http://www.omdbapi.com/?apikey=c270fbb2&s=${searchText}`)
+        .then((response) => {
+            console.log(response)
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie)=>{
+                output += `
+                    <div class = "col-md-3">
+                        <div class = "well text-center">
+                        <img src = "${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                        <a onclick = "movieSelected('${movie.imdbID}')"
+                        class="btn btn-primary"  href = "#">Movie Details</a>
+                    </div>
+                `
+            })
+
+            $('#movies').html(output);
+
+        })
+        .catch((err) => {
+            console.error("Error:", err.response ? err.response.data : err.message);
+        });
+}
+
